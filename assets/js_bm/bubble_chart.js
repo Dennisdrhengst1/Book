40a513e3e6139b02dad6c9d4d66ccbb280fa4f4c
Dennisdrhengst1 <lukas.ghirardin@bluewin.ch>
2025-03-07 11:08:08 +0100

    Add files via upload

--- assets/js_bm/bubble_chart.js
+++ assets/js_bm/bubble_chart.js
@@ -765,7 +765,7 @@ function moveToAgecat(alpha) {
 
   var fillColor = d3.scale.ordinal()
     .domain(['1','2','3', '4','5','6'])
-    .range(['#F7CAD0', '#ff006e', '#48CAE4', '#0096C7','#023E8A','#03045E']);
+    .range(['#F3FF33', '#D4FF33', '#A8FF33', '#83FF33','#064FF33','#42FF33']);
 
   /* Tooltip-Funktion*/
   function showDetail(d) {
